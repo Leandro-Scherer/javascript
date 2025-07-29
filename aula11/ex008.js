674ffb8324d286f43b3c7561d297815591c61562
Leandro Scherer <leandroscherer408@gmail.com>
2025-07-29 02:35:09 -0400

    criado ex e desafios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 120
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60){
+    console.log(`Voçê ultrapassou os limites de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
