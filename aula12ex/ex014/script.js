a35f1ac64cf95fd047aed3123e487715d23121a2
Leandro Scherer <leandroscherer408@gmail.com>
2025-08-01 00:42:52 -0400

    ex 14 criado

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function carregar() {
+  let msg = window.document.getElementById('msg')
+  let img = window.document.getElementById('imagem')
+  let data = new Date()
+  let hora = 5 // ou data.getHours()
+
+  msg.innerHTML = `Agora são ${hora} horas`
+
+  if(hora >= 0 && hora < 12){
+    img.src = 'imagens/imagem-manha.png'
+    document.body.style.background = '#e2cd9f'
+  } else if(hora >= 12 && hora < 18){
+    img.src = 'imagens/imagem-tarde.png'
+    document.body.style.background = '#b9846f'
+  } else {
+    img.src = 'imagens/imagem-noite.png'
+    document.body.style.background = '#515154'
+  }
+}
